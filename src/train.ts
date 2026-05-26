2f4dffe9dc1218b6578558d536551c14db150a8f
omadbek-mark <omadbekanvarbekov00@gmail.com>
2026-05-26 17:27:43 +0900

    feat: TASKs on train.ts

--- src/train.ts
+++ src/train.ts
@@ -11,4 +11,44 @@ function getSquareNumbers(arr: number[]) {
 }
 
 const result = getSquareNumbers([25, 15, 9]);
-console.log(result);
+console.log("TASK M:", result);
+
+
+// TASK L
+function reverseSentence(ele: string) {
+  let words = ele.split(" ");
+
+  let reversedWords = [];
+
+  for (let word of words) {
+    let ele1 = word.split("")
+    let getReverse = ele1.reverse();
+    let reversedSentence = getReverse.join("");
+    reversedWords.push(reversedSentence);
+  }
+
+  return reversedWords.join(" ");
+} 
+
+const result1 = reverseSentence("we are on typeScript now");
+console.log("TASK L:", result1);
+
+
+// TASK K
+function countVowels(word: string) {
+  let vowels = "aeiouAEIOU";
+  let founded_vowels = [];
+  let count = 0;
+
+  for (let letter of word) {
+    if (vowels.includes(letter)) {
+      count++;
+      founded_vowels.push(letter);
+    }
+  }
+
+  return {count, founded_vowels};
+}
+
+const result3 = countVowels("Never Give Up!")
+console.log("TASK K:", result3);
